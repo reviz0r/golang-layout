b400a9b9cd378f08ff471c25d6db17c63c2df58d
Nikita Kurochkin <nikurochkin@ozon.ru>
2020-01-25 14:11:33 +0300

    Add logger params

--- pkg/log/logger.go
+++ pkg/log/logger.go
@@ -1,23 +1,40 @@
 package log
 
 import (
+	"errors"
+	"io"
+
 	"github.com/sirupsen/logrus"
 	"go.uber.org/fx"
 )
 
 // Module register logger in DI container
-var Module = fx.Provide(NewLogger)
+var Module = fx.Provide(NewLogger, logrus.NewEntry)
 
 // LoggerParams .
 type LoggerParams struct {
 	fx.In
 
-	Level string `name:"log_level" optional:"true"`
+	Formatter string    `name:"log_formatter" optional:"true"`
+	Level     string    `name:"log_level" optional:"true"`
+	NoLock    bool      `name:"log_no_lock" optional:"true"`
+	Output    io.Writer `name:"log_output" optional:"true"`
 }
 
 // NewLogger gives new predefined logger
-func NewLogger(p LoggerParams) (*logrus.Entry, error) {
+func NewLogger(p LoggerParams) (*logrus.Logger, error) {
 	logger := logrus.New()
+
+	if p.Formatter != "" {
+		switch p.Formatter {
+		case "text":
+			logger.SetFormatter(new(logrus.TextFormatter))
+		case "json":
+			logger.SetFormatter(new(logrus.JSONFormatter))
+		default:
+			return nil, errors.New("unknown formatter")
+		}
+	}
 
 	if p.Level != "" {
 		level, err := logrus.ParseLevel(p.Level)
@@ -28,5 +45,13 @@ func NewLogger(p LoggerParams) (*logrus.Entry, error) {
 		logger.SetLevel(level)
 	}
 
-	return logrus.NewEntry(logger), nil
+	if p.NoLock {
+		logger.SetNoLock()
+	}
+
+	if p.Output != nil {
+		logger.SetOutput(p.Output)
+	}
+
+	return logger, nil
 }
