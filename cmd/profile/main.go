8a9af083c10128f906639876d112f69650615da8
Nikita Kurochkin <nikurochkin@ozon.ru>
2020-01-25 13:00:00 +0300

    Preview adopt app starting to fx

--- cmd/profile/main.go
+++ cmd/profile/main.go
@@ -2,23 +2,23 @@ package main
 
 import (
 	"context"
-	"database/sql"
 	"fmt"
 	"net"
 	"net/http"
 	"sync"
 
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
-	_ "github.com/lib/pq"
-	"github.com/sirupsen/logrus"
+	"go.uber.org/fx"
 	"google.golang.org/grpc"
 
-	internal "github.com/reviz0r/golang-layout/internal/profile"
+	"github.com/reviz0r/golang-layout/pkg/config"
 	"github.com/reviz0r/golang-layout/pkg/db"
 	"github.com/reviz0r/golang-layout/pkg/grace"
 	"github.com/reviz0r/golang-layout/pkg/log"
-	pkg "github.com/reviz0r/golang-layout/pkg/profile"
 	"github.com/reviz0r/golang-layout/pkg/server"
+
+	profile_internal "github.com/reviz0r/golang-layout/internal/profile"
+	profile_pkg "github.com/reviz0r/golang-layout/pkg/profile"
 )
 
 // TODO: make config
@@ -27,59 +27,26 @@ const (
 	httpPort = ":8081"
 )
 
-var (
-	logger   *logrus.Entry
-	database *sql.DB
-)
-
-func init() {
-	logger = log.NewLogger()
-	database = db.NewDatabase("golang-layout")
-}
-
 func main() {
-	logger.Debug("app starting")
+	app := fx.New(
+		config.Module,
+		log.Module,
+		db.Module,
+		server.Module,
+
+		// logic modules
+		profile_internal.Module,
+	)
+
+	go app.Run()
 
 	mainCtx, mainWg := grace.Grace()
-
-	// Start GRPC server
-	mainWg.Add(1)
-	go startGRPC(mainCtx, mainWg, grpcPort)
 
 	// Start GRPC-Gateway server
 	mainWg.Add(1)
 	go startHTTP(mainCtx, mainWg, httpPort, grpcPort, grpc.WithInsecure())
 
-	logger.Debug("app started")
-
 	mainWg.Wait()
-
-	logger.Debug("app is shutdown")
-}
-
-func startGRPC(ctx context.Context, wg *sync.WaitGroup, port string) {
-	defer wg.Done()
-
-	grpcServer := server.NewGrpcServer(logger)
-	pkg.RegisterUserServiceServer(grpcServer, &internal.UserService{DB: database})
-
-	go func() {
-		<-ctx.Done()
-		grpcServer.GracefulStop()
-		logger.Debug("grpc server is shutdown")
-	}()
-
-	logger.Debugf("Start grpc server on port %s", port)
-
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		panic(fmt.Errorf("cannot listen grpc port %s %v", port, err))
-	}
-
-	err = grpcServer.Serve(lis)
-	if err != nil {
-		panic(fmt.Errorf("cannot serve grpc: %v", err))
-	}
 }
 
 func startHTTP(ctx context.Context, wg *sync.WaitGroup, port, grpcPort string, opts ...grpc.DialOption) {
@@ -88,7 +55,7 @@ func startHTTP(ctx context.Context, wg *sync.WaitGroup, port, grpcPort string, o
 	protoMux := runtime.NewServeMux(
 		runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{EmitDefaults: true}))
 
-	err := pkg.RegisterUserServiceHandlerFromEndpoint(
+	err := profile_pkg.RegisterUserServiceHandlerFromEndpoint(
 		ctx, protoMux, "localhost"+grpcPort, opts)
 	if err != nil {
 		panic(fmt.Errorf("cannot register user service: %v", err))
@@ -107,10 +74,7 @@ func startHTTP(ctx context.Context, wg *sync.WaitGroup, port, grpcPort string, o
 	go func() {
 		<-ctx.Done()
 		s.Shutdown(ctx)
-		logger.Debug("http server is shutdown")
 	}()
-
-	logger.Debugf("Start http server on port %s", port)
 
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
