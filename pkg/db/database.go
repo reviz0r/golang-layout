28e95233cc9bb4389ab23fe5304500059e2fd502
Nikita Kurochkin <nikurochkin@ozon.ru>
2020-01-25 13:59:01 +0300

    Add database options to params

--- pkg/db/database.go
+++ pkg/db/database.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"time"
 
 	// import postgresql driver
 	_ "github.com/lib/pq"
@@ -17,20 +18,36 @@ var Module = fx.Provide(NewDatabase)
 type DatabaseParams struct {
 	fx.In
 
-	DatabaseDSN         string `name:"database_dsn"`
-	DatabasePingOnStart bool   `name:"database_ping_on_start"`
+	DSN             string        `name:"database_dsn"`
+	ConnMaxLifetime time.Duration `name:"database_conn_max_lifetime" optional:"true"`
+	MaxIdleConns    int           `name:"database_max_idle_conns" optional:"true"`
+	MaxOpenConns    int           `name:"database_max_open_conns" optional:"true"`
+
+	PingOnStart bool `name:"database_ping_on_start" optional:"true"`
 }
 
 // NewDatabase gives new predefined database connection
 func NewDatabase(lc fx.Lifecycle, p DatabaseParams) (*sql.DB, error) {
-	dbconn, err := sql.Open("postgres", p.DatabaseDSN)
+	dbconn, err := sql.Open("postgres", p.DSN)
 	if err != nil {
 		return nil, fmt.Errorf("cannot open connection to database: %v", err)
 	}
 
+	if p.ConnMaxLifetime != 0 {
+		dbconn.SetConnMaxLifetime(p.ConnMaxLifetime)
+	}
+
+	if p.MaxIdleConns != 0 {
+		dbconn.SetMaxIdleConns(p.MaxIdleConns)
+	}
+
+	if p.MaxOpenConns != 0 {
+		dbconn.SetMaxOpenConns(p.MaxOpenConns)
+	}
+
 	lc.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
-			if p.DatabasePingOnStart {
+			if p.PingOnStart {
 				err = dbconn.PingContext(ctx)
 				if err != nil {
 					return fmt.Errorf("cannot ping database connection: %v", err)
