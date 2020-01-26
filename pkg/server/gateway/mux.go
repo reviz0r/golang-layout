a9329cca96f718a3fe24153d0445e618418ab8e7
Nikita Kurochkin <nikurochkin@ozon.ru>
2020-01-26 15:12:31 +0300

    Modify gateway mux

--- pkg/server/gateway/mux.go
+++ pkg/server/gateway/mux.go
@@ -1,21 +1,31 @@
 package gateway
 
 import (
+	"github.com/golang/protobuf/jsonpb"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"go.uber.org/fx"
 )
 
-var MuxModule = fx.Provide(runtime.NewServeMux, NewGatewayMarshallerOption)
+var MuxModule = fx.Provide(runtime.NewServeMux, NewServeMuxMarshallerOption)
 
-type GatewayMarshallerParams struct {
+type ServeMuxMarshallerParams struct {
 	fx.In
 
-	EnumsAsInts  bool   `name:"gateway_marshaller_enums_as_ints" optional:"true"`
-	EmitDefaults bool   `name:"gateway_marshaller_emit_defaults" optional:"true"`
-	Indent       string `name:"gateway_marshaller_indent" optional:"true"`
-	OrigName     bool   `name:"gateway_marshaller_orig_name" optional:"true"`
+	EnumsAsInts  bool               `name:"gateway_marshaller_enums_as_ints" optional:"true"`
+	EmitDefaults bool               `name:"gateway_marshaller_emit_defaults" optional:"true"`
+	Indent       string             `name:"gateway_marshaller_indent" optional:"true"`
+	OrigName     bool               `name:"gateway_marshaller_orig_name" optional:"true"`
+	AnyResolver  jsonpb.AnyResolver `name:"gateway_marshaller_any_resolver" optional:"true"`
 }
 
-func NewGatewayMarshallerOption(p GatewayMarshallerParams) runtime.ServeMuxOption {
-	return runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{EmitDefaults: true})
+func NewServeMuxMarshallerOption(p ServeMuxMarshallerParams) runtime.ServeMuxOption {
+	marshaller := &runtime.JSONPb{
+		EnumsAsInts:  p.EnumsAsInts,
+		EmitDefaults: p.EmitDefaults,
+		Indent:       p.Indent,
+		OrigName:     p.OrigName,
+		AnyResolver:  p.AnyResolver,
+	}
+
+	return runtime.WithMarshalerOption(runtime.MIMEWildcard, marshaller)
 }
