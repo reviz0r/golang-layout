c2ffc962ac0ee6d4740695f9095081f4eed37807
Nikita Kurochkin <nikurochkin@ozon.ru>
2020-03-13 12:03:35 +0300

    Set default ports for grpc and http

--- configs/config.go
+++ configs/config.go
@@ -10,5 +10,7 @@ var Module = fx.Invoke(NewConfig)
 func NewConfig(config *viper.Viper) {
 	config.SetDefault("database.dsn", "host=localhost user=postgres sslmode=disable")
 	config.SetDefault("grpc.network", "tcp")
+	config.SetDefault("grpc.address", ":50051")
 	config.SetDefault("http.network", "tcp")
+	config.SetDefault("http.address", ":8081")
 }
